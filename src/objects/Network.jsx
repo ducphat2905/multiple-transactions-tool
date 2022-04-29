2155bdfeac961120cfd639019824adb027194fdf
phat@minim1 <phat.tran2905@gmail.com>
2022-04-28 18:23:00 -0700

    Add class Network

---
+++ src/objects/Network.jsx
@@ -0,0 +1,40 @@
+import Icon from "../components/Icon/Icon"
+import IconNames from "../components/Icon/IconNames"
+import BscIcon from "../assets/icons/bsc-icon.png"
+import TronIcon from "../assets/icons/tron-icon.png"
+
+class Network {
+    constructor({ id, name, blockExplorer, rpcEndpoint }) {
+        this.id = id
+        this.name = name
+        this.blockExplorer = blockExplorer
+        this.rpcEndpoint = rpcEndpoint
+    }
+
+    getIconComponent() {
+        let iconComponent
+
+        switch (this.id) {
+            case "bsc": {
+                iconComponent = <img src={BscIcon} alt="bsc-icon" width="15" height="15" />
+                break
+            }
+            case "ropsten":
+            case "ethereum": {
+                iconComponent = <Icon name={IconNames.FaEthereum} />
+                break
+            }
+            case "tron": {
+                iconComponent = <img src={TronIcon} alt="tron-icon" width="15" height="15" />
+                break
+            }
+            default:
+                iconComponent = ""
+                break
+        }
+
+        return iconComponent
+    }
+}
+
+export default Network
