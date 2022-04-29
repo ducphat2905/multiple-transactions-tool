85a5e74a9c9e731f299137d463a776620b9e12d0
phat@minim1 <phat.tran2905@gmail.com>
2022-04-28 18:26:07 -0700

    Create new file to stora local storage's names

--- src/constants.jsx
+++ src/constants.jsx
@@ -1,14 +1,43 @@
-export const DATA_TABLES = Object.freeze({
-    Collect: "collect-data-table",
-    Spread: "spread-data-table",
-    Wallet: "wallet-data-table"
+import Network from "./objects/Network"
+
+export const NETWORKS = [
+    new Network({
+        id: "ethereum",
+        name: "Ethereum",
+        blockExplorer: "https://etherscan.io/",
+        rpcEndpoint: ""
+    }),
+    new Network({
+        id: "bsc",
+        name: "Binance Smart Chain",
+        blockExplorer: "https://ropsten.etherscan.io/",
+        rpcEndpoint: ""
+    }),
+    new Network({
+        id: "tron",
+        name: "Tron Network",
+        blockExplorer: "https://tronscan.org/#/",
+        rpcEndpoint: ""
+    }),
+    new Network({
+        id: "ropsten",
+        name: "Ropsten (Testnet)",
+        blockExplorer: "https://www.bscscan.com/",
+        rpcEndpoint: ""
+    })
+]
+
+// Rate limit: 10k per 5min
+export const RPC_ENDPOINTS = Object.freeze({
+    Bsc: [
+        "https://bsc-dataseed.binance.org/",
+        "https://bsc-dataseed1.defibit.io/",
+        "https://bsc-dataseed1.ninicoin.io/"
+    ],
+    BscTestnet: [
+        "https://data-seed-prebsc-1-s1.binance.org:8545/",
+        "https://data-seed-prebsc-2-s1.binance.org:8545/"
+    ]
 })
 
-export const NETWORKS = Object.freeze({
-    Ethereum: { id: "ethereum", name: "Ethereum", blockExplorer: "" },
-    Bsc: { id: "bsc", name: "Binance Smart Chain", blockExplorer: "" },
-    Tron: { id: "tron", name: "Tron Network", blockExplorer: "" },
-    Ropsten: { id: "ropsten", name: "Ropsten (Testnet)", blockExplorer: "" }
-})
-
-export default { DATA_TABLES, NETWORKS }
+export default { NETWORKS, RPC_ENDPOINTS }
---
+++ src/redux/storageNames.jsx
@@ -0,0 +1,8 @@
+export const networkStorage = "network"
+export const settingStorage = "setting"
+
+export const DATA_TABLES = Object.freeze({
+    Collect: "collect-data-table",
+    Spread: "spread-data-table",
+    Wallet: "wallet-data-table"
+})
