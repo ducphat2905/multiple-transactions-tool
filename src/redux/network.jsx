40d1d00bdab394a3bcfa6356a5e80a9a5bbecfb6
phat@minim1 <phat.tran2905@gmail.com>
2022-04-28 18:29:00 -0700

    Get data from local storage for the initialState

--- src/redux/network.jsx
+++ src/redux/network.jsx
@@ -3,62 +3,56 @@ import EthereumTokens from "../tokens/ethereum"
 import BscTokens from "../tokens/bsc"
 import TronTokens from "../tokens/tron"
 import { NETWORKS } from "../constants"
+import { networkStorage } from "./storageNames"
 
-const initialState = {
-    storageName: "network",
-    id: "",
-    name: "",
-    blockExplorer: "",
-    tokens: []
+const initialState = () => {
+    // Default state
+    let state = {
+        storageName: networkStorage,
+        id: "",
+        name: "",
+        blockExplorer: "",
+        rpcEndpoint: "",
+        tokens: []
+    }
+
+    // Get from local storage
+    const networkData = localStorage.getItem(networkStorage)
+    if (networkData) {
+        const storageData = JSON.parse(networkData)
+        state = storageData
+    }
+
+    return state
 }
 
 export const networkSlice = createSlice({
     name: "network",
-    initialState,
+    initialState: initialState(),
     reducers: {
         setNetwork: (state, action) => {
-            const networkId = action.payload
-            const network = Object.entries(NETWORKS).find(
-                ([key, value]) => value.id === networkId
-            )[1]
-            const { id, name, blockExplorer } = network
-            // Update state
-            state.id = id
-            state.name = name
-            state.blockExplorer = blockExplorer
-            // Store in localStorage
-            localStorage.setItem(
-                state.storageName,
-                JSON.stringify({
-                    id,
-                    name,
-                    blockExplorer
-                })
-            )
-        },
-        getChosenNetwork: (state) => {
-            const network = localStorage.getItem(state.storageName)
-            // Set network if existed
-            if (network) {
-                const { id, name, blockExplorer } = JSON.parse(network)
-                state.id = id
-                state.name = name
-                state.blockExplorer = blockExplorer
+            const { id } = action.payload
+
+            const selectedNetwork = NETWORKS.find((_network) => _network.id === id)
+            if (selectedNetwork) {
+                // Update state
+                state = { ...state, ...selectedNetwork }
+                // Store in localStorage
+                localStorage.setItem(state.storageName, JSON.stringify(state))
             }
         },
         getTokens: (state) => {
-            const { Ethereum, Ropsten, Bsc, Tron } = NETWORKS
             switch (state.id) {
-                case Bsc.id: {
+                case "bsc": {
                     state.tokens = [...BscTokens]
                     break
                 }
-                case Ropsten.id:
-                case Ethereum.id: {
+                case "ropsten":
+                case "ethereum": {
                     state.tokens = [...EthereumTokens]
                     break
                 }
-                case Tron.id: {
+                case "tron": {
                     state.tokens = [...TronTokens]
                     break
                 }
@@ -71,6 +65,6 @@ export const networkSlice = createSlice({
 })
 
 // Action creators are generated for each case reducer function
-export const { setNetwork, getChosenNetwork, getTokens } = networkSlice.actions
+export const { setNetwork, getTokens } = networkSlice.actions
 
 export default networkSlice.reducer
