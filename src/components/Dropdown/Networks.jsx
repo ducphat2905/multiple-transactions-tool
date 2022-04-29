74f2a9eaf0ce0d43b718dd3e23e6852d383cc83e
phat@minim1 <phat.tran2905@gmail.com>
2022-04-28 18:30:30 -0700

    Split dropdown to another component

---
+++ src/components/Dropdown/Networks.jsx
@@ -0,0 +1,59 @@
+import Dropdown from "react-bootstrap/Dropdown"
+import ButtonGroup from "react-bootstrap/ButtonGroup"
+import Button from "react-bootstrap/Button"
+import { useEffect, useState } from "react"
+import PropTypes from "prop-types"
+import { useSelector } from "react-redux"
+import Network from "../../objects/Network"
+
+function Networks({ selectHandler }) {
+    const setting = useSelector((state) => state.setting)
+    const selectedNetwork = useSelector((state) => state.network)
+    const [network, setNetwork] = useState(() => new Network({ ...selectedNetwork }))
+    const [networks] = useState(() =>
+        setting.networks.map((_network) => new Network({ ..._network }))
+    )
+
+    useEffect(() => {
+        // Set chosen network for navbar
+        if (selectedNetwork.id && selectedNetwork.id !== network.id) {
+            setNetwork(new Network({ ...selectedNetwork }))
+        }
+    }, [selectedNetwork.id])
+
+    const selectNetwork = (_network) => {
+        setNetwork(_network)
+        selectHandler(_network)
+    }
+
+    return (
+        <Dropdown as={ButtonGroup}>
+            <Button variant={network.id ? "info" : "danger"}>
+                {network.id && network.getIconComponent()}
+
+                <span className="mx-2">{network.id ? `${network.name}` : "Choose a network"}</span>
+            </Button>
+
+            <Dropdown.Toggle
+                split
+                variant={network.id ? "info" : "danger"}
+                id="dropdown-split-basic"
+            />
+
+            <Dropdown.Menu>
+                {networks.map((_network) => (
+                    <Dropdown.Item key={_network.id} onClick={() => selectNetwork(_network)}>
+                        <span className="mx-1">{_network.getIconComponent()}</span>
+                        {_network.name}
+                    </Dropdown.Item>
+                ))}
+            </Dropdown.Menu>
+        </Dropdown>
+    )
+}
+
+Networks.propTypes = {
+    selectHandler: PropTypes.func.isRequired
+}
+
+export default Networks
--- src/components/Nav/NavigationBar.jsx
+++ src/components/Nav/NavigationBar.jsx
@@ -1,83 +1,33 @@
 import Container from "react-bootstrap/Container"
 import Navbar from "react-bootstrap/Navbar"
 import Nav from "react-bootstrap/Nav"
-import ButtonGroup from "react-bootstrap/ButtonGroup"
-import Button from "react-bootstrap/Button"
-import Dropdown from "react-bootstrap/Dropdown"
 
 import { useLocation, useSearchParams } from "react-router-dom"
-import { useEffect, useRef, useMemo, useCallback } from "react"
+import { useEffect } from "react"
 import { useSelector, useDispatch } from "react-redux"
 
-import { setNetwork, getChosenNetwork } from "../../redux/Network"
-import { setProvider } from "../../redux/Ethereum"
+import { setNetwork } from "../../redux/Network"
 
 import logo from "../../logo.svg"
-import Icons from "../Icon/Icons"
-import BscIcon from "../Icon/bsc-icon.png"
-import TronIcon from "../Icon/tron-icon.png"
-import { NETWORKS } from "../../constants"
-
-const { Ethereum, Bsc, Tron, Ropsten } = NETWORKS
+import Networks from "../Dropdown/Networks"
 
 function NavigationBar() {
     const location = useLocation()
     const [, setSearchParams] = useSearchParams()
     const network = useSelector((state) => state.network)
-    const { provider } = useSelector((state) => state.ethereum)
     const dispatch = useDispatch()
-    const selectedNetworkRef = useRef()
 
     useEffect(() => {
-        // Get chosen network from local storage
-        dispatch(getChosenNetwork())
-        // Set provider
-        dispatch(
-            setProvider("https://ropsten.infura.io/v3/640d8cc3f12148e6b511453827f6c57c")
-        )
-        // Set search params from selected network in the local storage
-        if (network.id) setSearchParams({ networkId: network.id })
+        if (network.id) {
+            // Add chosen network to URL
+            setSearchParams({ networkId: network.id })
+        }
     }, [network])
 
-    const selectNetwork = (networkId) => {
-        dispatch(setNetwork(networkId))
-        setSearchParams({ networkId })
+    const selectNetwork = (_network) => {
+        dispatch(setNetwork({ ..._network }))
+        setSearchParams({ networkId: _network.id })
     }
-
-    const displaySelectedNetwork = useCallback(
-        (networkId) => {
-            switch (networkId) {
-                case Bsc.id:
-                    return <img src={BscIcon} alt="bsc-icon" width="15" height="15" />
-                case Ethereum.id:
-                case Ropsten.id:
-                    return <Icons iconName="FaEthereum" />
-                case Tron.id:
-                    return <img src={TronIcon} alt="bsc-icon" width="15" height="15" />
-                default:
-                    return ""
-            }
-        },
-        [network]
-    )
-
-    const networks = useMemo(() => {
-        return [Bsc, Ethereum, Tron, Ropsten].map(({ id, name }) => (
-            <Dropdown.Item key={id} onClick={() => selectNetwork(id)}>
-                {[Ropsten.id, Ethereum.id].includes(id) && (
-                    <Icons iconName="FaEthereum" className="mx-1" />
-                )}
-
-                {[Bsc.id].includes(id) && (
-                    <img src={BscIcon} alt="bsc-icon" width="15" height="15" className="mx-1" />
-                )}
-                {[Tron.id].includes(id) && (
-                    <img src={TronIcon} alt="tron-icon" width="15" height="15" className="mx-1" />
-                )}
-                {name}
-            </Dropdown.Item>
-        ))
-    }, [])
 
     return (
         <Navbar bg="dark" variant="dark">
@@ -111,25 +61,7 @@ function NavigationBar() {
 
                 <Nav>
                     <Nav.Item>
-                        <Dropdown as={ButtonGroup}>
-                            <Button
-                                ref={selectedNetworkRef}
-                                variant={network.id ? "info" : "danger"}>
-                                {network.id && displaySelectedNetwork(network.id)}
-
-                                <span className="mx-2">
-                                    {network.id ? `${network.name}` : "Choose a network"}
-                                </span>
-                            </Button>
-
-                            <Dropdown.Toggle
-                                split
-                                variant={network.id ? "info" : "danger"}
-                                id="dropdown-split-basic"
-                            />
-
-                            <Dropdown.Menu>{networks}</Dropdown.Menu>
-                        </Dropdown>
+                        <Networks selectHandler={selectNetwork} />
                     </Nav.Item>
                 </Nav>
             </Container>
