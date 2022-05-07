f94b6fe1b2ed052e021f8dcfc4a546ad0569565d
phat@minim1 <phat.tranducvn@gmail.com>
2022-05-06 23:24:39 -0700

    Use stage as a condition to display components

--- src/pages/Collect.jsx
+++ src/pages/Collect.jsx
@@ -3,12 +3,37 @@ import Col from "react-bootstrap/Col"
 import { useState, useEffect } from "react"
 import { useSelector, useDispatch } from "react-redux"
 import DataTable from "../components/DataTable/DataTable"
-import DropFile from "../components/DropFile/DropFile"
+import DropFileComponent from "../components/DropFile/DropFile"
 import { getDataTable, setStorageName } from "../redux/DataTable"
 import { DATA_TABLES } from "../redux/storageNames"
+import { TOOL_STAGES } from "../constants"
+
+function DropFile() {
+    return <DropFileComponent />
+}
+
+function InputTable() {
+    return <DataTable />
+}
+
+function CollectForm() {
+    return <h1>Collect Form</h1>
+}
+
+function SpreadForm() {
+    return <h1>Spread Form</h1>
+}
+
+function Logging() {
+    return <h1>Logging</h1>
+}
+
+function ResultTable() {
+    return <DataTable />
+}
 
 function Collect() {
-    const [hasData, setHasData] = useState(false)
+    const [stage, setStage] = useState(TOOL_STAGES.DropFile)
     const { rows, columns } = useSelector((state) => state.dataTable)
     const dispatch = useDispatch()
 
@@ -21,13 +46,20 @@ function Collect() {
     }, [])
 
     useEffect(() => {
-        if (rows.length > 0) setHasData(true)
-        else setHasData(false)
+        if (rows.length > 0) setStage(TOOL_STAGES.InputTable)
+        else setStage(TOOL_STAGES.DropFile)
     }, [rows, columns])
 
     return (
         <Row>
-            <Col className="pb-5 px-4">{hasData ? <DataTable /> : <DropFile />}</Col>
+            <Col className="pb-5 px-4">
+                {stage === TOOL_STAGES.DropFile && <DropFile />}
+                {stage === TOOL_STAGES.InputTable && <InputTable />}
+                {stage === TOOL_STAGES.CollectForm && <CollectForm />}
+                {stage === TOOL_STAGES.SpreadForm && <SpreadForm />}
+                {stage === TOOL_STAGES.Logging && <Logging />}
+                {stage === TOOL_STAGES.ResultTable && <ResultTable />}
+            </Col>
         </Row>
     )
 }
