8161cd1abd7f27f3ddb3289ba4259ab99f692202
phat@minim1 <phat.tranducvn@gmail.com>
2022-05-06 23:20:18 -0700

    Making footer fixed-bottom depending on location

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,15 @@
 import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
+import { useLocation } from "react-router-dom"
 
 function Footer() {
+    const location = useLocation()
+
     return (
-        <Row className="bg-dark fixed-bottom">
+        <Row
+            className={`bg-dark ${
+                ["/", "/setting"].includes(location.pathname) && "fixed-bottom"
+            }`}>
             <Col className="text-white text-center p-2">&copy; 2022 Copyright: phatductran</Col>
         </Row>
     )
