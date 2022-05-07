74b18f4e35340a42f3a8ab81bd9af8271e966405
phat@minim1 <phat.tranducvn@gmail.com>
2022-05-06 23:22:51 -0700

    Add name for result's table

--- src/redux/storageNames.jsx
+++ src/redux/storageNames.jsx
@@ -4,5 +4,6 @@ export const settingStorage = "setting"
 export const DATA_TABLES = Object.freeze({
     Collect: "collect-data-table",
     Spread: "spread-data-table",
-    Wallet: "wallet-data-table"
+    Wallet: "wallet-data-table",
+    Result: "result-data-table"
 })
