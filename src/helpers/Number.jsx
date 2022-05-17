a00553141cc6066745ae58139cfcc93445f53037
phat@minim1 <phat.tranducvn@gmail.com>
2022-05-17 12:01:52 -0700

    Fix: Throw an error when not passing the decimal

--- src/helpers/Number.jsx
+++ src/helpers/Number.jsx
@@ -1,8 +1,10 @@
 const parseToDecimalVal = (num, decimal) => {
+    if (!decimal) throw new Error("decimal is not found")
     return num / 10 ** decimal
 }
 
 const parseFromDecimalVal = (num, decimal) => {
+    if (!decimal) throw new Error("decimal is not found")
     return num * 10 ** decimal
 }
 
